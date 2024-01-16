07cc637060d1634c518364d48f6327f93ec04fe8
nikolav <admin@nikolav.rs>
2024-01-16 20:27:02 +0000

    Tue Jan 16 21:27:02 CET 2024

--- api.py
+++ api.py
@@ -16,6 +16,6 @@ if __name__ == '__main__':
   # app.run(debug = True)
   io.run(app, 
          debug = True,
-         host = '0.0.0.0',
-         port = 5000,
+        #  host = '0.0.0.0',
+        #  port = 5000,
          allow_unsafe_werkzeug = True)
