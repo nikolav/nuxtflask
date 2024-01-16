196b74f47a7735b6d2618dfd9dff45e9df1fe4b0
nikolav <admin@nikolav.rs>
2024-01-16 21:31:17 +0000

    Tue Jan 16 22:31:16 CET 2024

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import os
 
 from flask_app import app, api, db, io
 from resources.docs import DocsResource
@@ -14,8 +15,12 @@ if __name__ == '__main__':
   with app.app_context():
     db.create_all()
   # app.run(debug = True)
+
+  # auto assigned heroku app port
+  _port = os.getenv('PORT')
+
   io.run(app, 
          debug = True,
          host = '0.0.0.0',
-         port = 40117,
+         port = _port if None != _port else 5000,
          allow_unsafe_werkzeug = True)
