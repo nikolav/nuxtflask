dc0ff0429babe8b3eaec2a1434aefbfdefe5c325
nikolav <admin@nikolav.rs>
2024-01-18 19:48:39 +0000

    flaskapi:zip-utils

--- flaskapi/blueprints/__init__.py
+++ flaskapi/blueprints/__init__.py
@@ -1,4 +1,3 @@
-import os
 import json
 
 from flask import Blueprint
@@ -6,19 +5,27 @@ from flask import Blueprint
 from models.tags import Tags
 
 
+TAG_VARS = '@vars'
+
 bp_home = Blueprint('home', __name__, url_prefix = '/')
 
 @bp_home.route('/', methods = ['GET'])
 def status_ok():
-  app_name = ''
-  tag = Tags.query.filter(Tags.tag == '@vars').first()
+  
+  admin_email = ''
+  app_name    = ''
+  
+  tag = Tags.query.filter(Tags.tag == TAG_VARS).first()
   for d in tag.docs:
     data = json.loads(d.data)
     if 'app:name' in data:
       app_name = data['app:name']
+    if 'admin:email' in data:
+      admin_email = data['admin:email']
+    if app_name and admin_email:
       break
     
-  return { 
-    'app:name' : app_name if app_name else os.getenv('APP_NAME'),
-    'status'   : 'ok',
+  return {
+    'admin:email' : admin_email,
+    'app:name'    : app_name,
   }
