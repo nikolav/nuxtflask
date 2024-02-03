3ac6d6fa6b4b08e6529572f68d6542267d9df80e
nikolav <admin@nikolav.rs>
2024-02-03 16:22:35 +0000

    Sat Feb  3 16:22:28 UTC 2024

--- flaskapi/config/init_tables.py
+++ flaskapi/config/init_tables.py
@@ -10,36 +10,28 @@ from utils.pw    import hash as hashPassword
 
 
 for t in init_docs_tags:
-  try:
-    Tags.by_name(t, create = True)
-  except:
-    pass
+  Tags.by_name(t, create = True)
 
+tag_vars = Tags.by_name(TAG_VARS)
 
-try:
-  tag = Tags.by_name(TAG_VARS)
+vars_data = [doc.data for doc in tag_vars.docs]
+
+if all(not 'app:name' in node for node in vars_data):
+  tag_vars.docs.append(Docs(data = { 'app:name': "app:nuxtflask" }))
+
+if all(not 'admin:email' in node for node in vars_data):
+  tag_vars.docs.append(Docs(data = { 'admin:email': "admin@nikolav.rs" }))
   
-  if tag:  
-    vars_data = [doc.data for doc in tag.docs]
-    
-    if all(not 'app:name' in node for node in vars_data):
-      tag.docs.append(Docs(data = { 'app:name': "app:nuxtflask" }))
+db.session.commit()
 
-    if all(not 'admin:email' in node for node in vars_data):
-      tag.docs.append(Docs(data = { 'admin:email': "admin@nikolav.rs" }))
-  
-except:
-  pass
-else:
-  db.session.commit()
 
 email_    = os.getenv('ADMIN_EMAIL')
 password_ = os.getenv('ADMIN_PASSWORD')
 
 docAdmin  = None
-tagUsers  = Tags.by_name(TAG_USERS);
+tag_users  = Tags.by_name(TAG_USERS);
 
-for d in tagUsers.docs:
+for d in tag_users.docs:
   if email_ == d.data['email']:
     docAdmin = d
     break
@@ -49,40 +41,24 @@ if not docAdmin:
                 'email': email_, 
                 'password': hashPassword(password_) 
               })
-  tagUsers.docs.append(docAdmin)
+  tag_users.docs.append(docAdmin)
   db.session.add(docAdmin)
-  db.session.commit()
+
+db.session.commit()
 
 
 tagPolicyADMINS = Tags.by_name(os.getenv('POLICY_ADMINS'), create = True)
 tagPolicyEMAIL  = Tags.by_name(os.getenv('POLICY_EMAIL'), create = True)
 tagPolicyFS     = Tags.by_name(os.getenv('POLICY_FILESTORAGE'), create = True)
-# tagPolicyALL    = Tags.by_name(os.getenv('POLICY_ALL'), create = True)
+tagPolicyALL    = Tags.by_name(os.getenv('POLICY_ALL'), create = True)
 
-try:
+if not docAdmin.includes_tags(os.getenv('POLICY_ADMINS')):
   tagPolicyADMINS.docs.append(docAdmin)
-except:
-  pass
-else:
-  db.session.commit()
-
-try:
+if not docAdmin.includes_tags(os.getenv('POLICY_EMAIL')):
   tagPolicyEMAIL.docs.append(docAdmin)
-except:
-  pass
-else:
-  db.session.commit()
-
-try:
+if not docAdmin.includes_tags(os.getenv('POLICY_FILESTORAGE')):
   tagPolicyFS.docs.append(docAdmin)
-except:
-  pass
-else:
-  db.session.commit()
-
-# try:
+# if not docAdmin.includes_tags(os.getenv('POLICY_ALL')):
 #   tagPolicyALL.docs.append(docAdmin)
-# except:
-#   pass
-# else:
-#   db.session.commit()
+
+db.session.commit()
